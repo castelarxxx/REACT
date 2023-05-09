f55618b4fab76ff8cb624175ef014941576cf7dc
Hilary Castelar <108029132+castelarxxx@users.noreply.github.com>
2023-05-09 11:52:45 -0300

    Add files via upload

---
+++ atividadecompleta.js
@@ -0,0 +1,27 @@
+/*
+ Crie um variável preço e atribua a ela um valor numérico. Em seguida, crie um 
+variável desconto e atribua a ela um valor 0.2, representando a porcentagem (20%) de 
+desconto que você deseja aplicar. Por fim, crie uma variável Preço final que use as variáveis 
+preço e desconto para calcular o preço com desconto e imprima o resultado no console.
+
+IF – ELSE
+
+5 Faça um programa conferir sua idade. Se a idade for menor do que 18 anos, exiba 
+uma mensagem informando que o usuário é menor de idade. Se a idade for igual ou maior 
+do que 18 anos e menor do que 60 anos, exiba uma mensagem informando que o usuário 
+é adulto. Se a idade for igual ou maior do que 60 anos, exiba uma mensagem informando 
+que o usuário é idoso.
+
+6 Faça um programa que teste a idade do usuário. Se a idade digitada for maior ou 
+igual a 18, exiba a mensagem "Você pode dirigir". Caso contrário, exiba a mensagem "Você 
+ainda não pode dirigir".
+
+7 Faça um programa que peça ao usuário para digitar um número inteiro. Se o número 
+for positivo, exiba uma mensagem informando que o número é positivo. Se o número for 
+negativo, exiba uma mensagem informando que o número é negativo. Se o número for 
+igual a zero, exiba uma mensagem informando que o número é zero. (Aplicar no Console).
+8 Atividades relacionais: crie um programa que solicite ao usuário que digite a sua 
+altura em metros e o seu peso em quilogramas. Em seguida, o programa deve calcular o 
+índice de massa corporal (IMC) do usuário usando a fórmula IMC = peso / altura*2 e 
+verificar se o IMC está dentro do intervalo saudável de 18,5 a 24,9. O programa deve 
+imprimir na tela se o IMC está dentro do intervalo saudável ou não*/
